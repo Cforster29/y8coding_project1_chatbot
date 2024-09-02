04714c6fe30b1028492e8dcece7d5e1bf1e02922
Cforster29 <cforster29@kellettschool.com>
2024-09-02 07:00:17 +0000

    my work my work

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-# your chatbot code here!
+name = input ("what is your name?")
+if name == "caspar":
+    print("oh, its just me.")
+else:
+    print("its not me.")
+
+siblings = int(input("how many siblings do you have?"))
+if siblings == 0:
+    print("U an only child???")
+elif siblings >= 5:
+    ("Wowie.")
